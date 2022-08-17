a7837ff48a34fa71b4c3f674d1a5b3fdf49ca9b7
Mohammad99Azim <103434512+Mohammad99Azim@users.noreply.github.com>
2022-08-17 09:19:53 +0300

    first try deploy with docker

--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    pass
-    # add additional fields in here
 
     def __str__(self):
         return self.username
